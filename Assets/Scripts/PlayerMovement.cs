62468d62387e81196ee4a694652ea9fd978cfad3
AlexMercer01 <9ampjm@gmail.com>
2014-11-03 22:37:48 +0400

    Sripts

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour {
+	public float startSpeed=10;
+	public float maxSpeed=100;
+	public float a=1;
+	public float curSpeed;
+	public float sideSpeed=10;
+
+	// Use this for initialization
+	void Start () {
+		curSpeed = startSpeed;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 movement = new Vector3 (curSpeed, 0, 0);
+		rigidbody.AddForce (movement * curSpeed * Time.deltaTime);
+	}
+}
