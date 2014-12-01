61078e59c1cbe1c4b3aedd7eb6ed88bbd2665c92
AlexMercer01 <9ampjm@gmail.com>
2014-12-01 13:09:28 +0400

    PickUps

--- Assets/Scripts/PickUpsScript.cs
+++ Assets/Scripts/PickUpsScript.cs
@@ -2,7 +2,11 @@
 using System.Collections;
 
 public class PickUpsScript : MonoBehaviour {
-
+	public GameObject prefab;
+	public GameObject player;
+	GameObject instance;
+	int i=30;
+	int k=1;
 	// Use this for initialization
 	void Start () {
 	
@@ -10,6 +14,16 @@ public class PickUpsScript : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-	
+		if (k == 1) {
+			float positionX = Random.Range (-5, 5);
+			float positionY = Random.Range (-5, 5);
+			instance = Instantiate (prefab) as GameObject;
+			instance.transform.position = new Vector3 (positionX, positionY, i);
+			i = i + 60;
+			k = 0;
+		}
+		if (player.transform.position.z > instance.transform.position.z) {
+			k = 1;
+		}
 	}
 }
